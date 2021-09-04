d4d189a5756d309d41f63d06b3a0566f31aef7bf
Arvind Sangwan <asangwan8998@gmail.com>
2021-09-04 09:45:11 +0530

    Added NavBar setup changes and responsive react-responsive library changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import tw from "twin.macro"
+import { HomePage } from './app/Containers/HomePage';
 
+const AppContainer = styled.div`
+  ${tw`flex flex-col w-full h-full`}
+`;
 function App() {
   return (
     <div className="App">
-      Hello World
+      <HomePage />
     </div>
   );
 }
