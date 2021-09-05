ea32514fe845f05e6c2586c4e078c3453e5af576
Arvind Sangwan <asangwan8998@gmail.com>
2021-09-05 22:58:42 +0530

    Added changes

--- src/app/Containers/HomePage/index.tsx
+++ src/app/Containers/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import tw from "twin.macro"
 import NavBar from "../../Components/Navbar"
+import  TopSection  from "./TopSection"
 
 const PageContainer = styled.div`
   ${tw`flex flex-col w-full h-full items-center overflow-x-hidden`}
@@ -11,6 +12,7 @@ export function HomePage() {
   return (
     <PageContainer>
       <NavBar />
+      <TopSection />
     </PageContainer>
   )
 }
