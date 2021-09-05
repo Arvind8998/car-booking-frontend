32d50f83e9bb91536cb6dbbb29738d0cc237d83f
Arvind Sangwan <asangwan8998@gmail.com>
2021-09-05 17:50:35 +0530

    Added changes for ts project tailwind css

--- src/app/Components/Navbar/navItems.tsx
+++ src/app/Components/Navbar/navItems.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import tw from "twin.macro"
 import { slide as Menu } from "react-burger-menu"
 import { useMediaQuery } from "react-responsive"
@@ -10,8 +10,13 @@ const ListContainer = styled.ul`
   ${tw`flex list-none`}
 `
 
-const NavItem = styled.li`
-  ${tw`text-xs md:text-base text-black font-medium mr-1 md:mr-5 cursor-pointer transition duration-300 ease-in-out hover:text-gray-700`}
+const NavItem = styled.li<{ menu?: any }>`
+  ${tw`text-xs md:text-black text-black font-medium mr-1 md:mr-5 cursor-pointer transition duration-300 ease-in-out hover:text-gray-700`}
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw` text-white text-xl mb-3 focus: text-white`}
+    `}
 `
 
 export function NavItems() {
@@ -20,16 +25,16 @@ export function NavItems() {
     return (
       <Menu right styles={menuStyles}>
         <ListContainer>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Home</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Cars</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Services</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Contact Us</a>
           </NavItem>
         </ListContainer>
